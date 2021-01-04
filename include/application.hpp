ade789a3ac8edf60d47fcfecd19476c2d3c6e3d3
rubot813 <soganov.sasha@yandex.ru>
2021-01-05 01:03:51 +0400

    try to fix broken rus symbols wtf?

--- include/application.hpp
+++ include/application.hpp
@@ -9,54 +9,54 @@
 #include "figure.hpp"
 
 /*
-	 ласс описывает логику работы приложени¤
-	«десь реализуетс¤ логика, отрисовка
+	Класс описывает логику работы приложения
+	здесь реализуется логика, отрисовка
 */
 
 class application {
 	public:
 
-		// ¬с¤ логика реализована в конструкторе
+		// Вся логика реализована в конструкторе
 		application( void );
 		~application( void );
 
 	private:
 
-		// ћетод инициализации, вернет true в случае успеха
+		// Метод инициализации, вернет true в случае успеха
 		bool _init( void );
 
-		// ћетоды обработки логики и рендера
+		// Методы обработки логики и рендера
 		void _logic( void );
 		void _render( void );
 
 		void _render_text( sf::Vector2f position, std::string text );
 
-		// ‘ункци¤ заполнени¤ пол¤ пикселей с экрана
-		// ѕринимает указатель на поле, координаты начала пол¤ и смещение по ос¤м x, y
+		// Функция заполнения поля пикселей с экрана
+		// Принимает указатель на поле, координаты начала пол¤ и смещение по осям x, y
 		void _fill_pixel_field_from_screen( pixel_field_c *pixel_field, sf::Vector2i start_pos, sf::Vector2i offset );
 
-		// ѕеременные SFML
+		// Переменные SFML
 		sf::RenderWindow	*_sf_render_window;
 		sf::Event			*_sf_event;
 		sf::RectangleShape	*_sf_rect_shape;
 		sf::Font			*_sf_font;
 		sf::Text			*_sf_text;
 
-		// ƒескриптор окна, сам ¤вл¤етс¤ указателем
+		// Дескриптор окна, сам является указателем
 		HWND				_window_hwnd;
 
-		// ѕеременные пол¤, с которым работаем
-		// ќбновл¤ютс¤ каждую итерацию
-		pixel_field_c	*_pf;		// ÷вета
-		cell_field_c	*_cf;		// «начени¤ ¤чеек
+		// Переменные поля, с которым работаем
+		// обновляются каждую итерацию
+		pixel_field_c	*_pf;		// Цвета
+		cell_field_c	*_cf;		// Значения ячеек
 
-		// * ¬ данной реализации AI знает только о текущей фигуре
-		// ѕеременные фигуры
-		pixel_field_c	*_figure_pf;	// ÷вета
-		cell_field_c	*_figure_cf;	// «начени¤ ¤чеек
-		figure_c		*_figure;		// “ип фигуры
+		// В данной реализации AI знает только о текущей фигуре
+		// Переменные фигуры
+		pixel_field_c	*_figure_pf;	// Цвета
+		cell_field_c	*_figure_cf;	// Значения ячеек
+		figure_c		*_figure;		// Тип фигуры
 
-		// Ќастроки цветов дл¤ online-tetris.ru
+		// Настроки цветов для online-tetris.ru
 		settings_s 		*_online_tetris_settings;
 };
 
